cde07b465146d35b93aea210a7f225fc29850001
hamidzharfan <92714460+hamidzharfan@users.noreply.github.com>
2022-03-13 09:36:03 +0700

    Add files via upload

---
+++ tugas web 02/array02.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<form>
+  <div class="form-group row">
+    <label for="" class="col-4 col-form-label">Nama</label> 
+    <div class="col-8">
+      <input id="" name="" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="select" class="col-4 col-form-label">matakuliah</label> 
+    <div class="col-8">
+      <select id="select" name="select" class="custom-select">
+        <option value="">dasar dasar pemograman</option>
+        <option value="">pemograman web</option>
+        <option value="">jaringan komputer</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="" class="col-4 col-form-label">Nilai uts</label> 
+    <div class="col-8">
+      <input id="" name="" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text1" class="col-4 col-form-label">Nilai uas</label> 
+    <div class="col-8">
+      <input id="text1" name="text1" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text2" class="col-4 col-form-label">Nilai Tugas/praktikum</label> 
+    <div class="col-8">
+      <input id="text2" name="text2" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+</body>
+</html>
